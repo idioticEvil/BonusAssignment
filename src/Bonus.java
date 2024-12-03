d1f45c9ad71901a1c19b6625bc74e017d0a3d3c0
Walter Mink <wwmink@crimson.ua.edu>
2024-12-03 11:26:23 -0600

    No longer stores password in plain text! Hooray1

--- src/Bonus.java
+++ src/Bonus.java
@@ -3,8 +3,44 @@ import java.io.*;
 
 public class Bonus {
     public static void main(String[] args) throws Exception {
-        System.out.println("Connecting to database...");
-        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@//oracle.cs.ua.edu:1521/xe", "wwmink", "Gumcat11.exe");
-        System.out.println("Connection Established successfully");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean connected = false;
+        boolean userDisconnect = false;
+        Connection conn = null;
+
+        while (!connected) {
+            System.out.println("Welcome, please enter your username");
+            String username = reader.readLine();
+
+            System.out.println("Please enter your password");
+            String password = reader.readLine();
+
+            try {
+                conn = DriverManager.getConnection("jdbc:oracle:thin:@//oracle.cs.ua.edu:1521/xe", username, password);
+                System.out.println("Connection Established successfully");
+                connected = true;
+            } catch (SQLException e) {
+                System.out.println("Incorrect credentials. Would you like to try again? (yes/no)");
+                String choice = reader.readLine();
+                
+                if (!choice.equalsIgnoreCase("yes")) {
+                    System.out.println("Exiting program.");
+                    System.exit(0);
+                }
+            }
+        }
+
+        while(!userDisconnect) {
+            Statement stmt = conn.createStatement();
+            System.out.println("Please enter an employee SSN or Project Number. To exit, type '0'");
+            String input = reader.readLine();
+
+            if (input.equals("0")) {
+                userDisconnect = true;
+                System.out.println("Exiting program.");
+                conn.close();
+                System.exit(0);
+            }
+        }
     }
 }
